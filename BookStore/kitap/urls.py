1bb47140e4f08261702a1a3dfb7b4c841e2cb4ce
Aisayev Duman <duman200354@mail.ru>
2022-05-02 15:33:00 +0600

    html structure

--- BookStore/kitap/urls.py
+++ BookStore/kitap/urls.py
@@ -2,5 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('authentication', views.authentication, name="authentication"), 
+    path('', views.index, name="index"), 
+    path('about', views.about, name="about"),
+    path('categories', views.categories, name="categories"),
+    path('profile', views.profile, name="profile"),
+    path('faq', views.faq, name="faq"),
 ]
--- BookStore/kitap/views.py
+++ BookStore/kitap/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def authentication(request):
+    return render(request, 'kitap/authentication.html')
+
 def index(request):
     return render(request, 'kitap/index.html')
+
+def about(request):
+    return render(request, 'kitap/about.html')
+
+def categories(request):
+    return render(request, 'kitap/categories.html')
+
+def profile(request):
+    return render(request, 'kitap/profile.html')
+
+def faq(request):
+    return render(request, 'kitap/faq.html')
